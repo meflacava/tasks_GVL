80ba365381bb0e576352f4604304020a38df00d4
Melanie E.F. LaCava <meflacava@users.noreply.github.com>
2026-02-20 11:27:30 -0800

    Add LCT files

---
+++ LCT_HybridPanel_AlleleCounting.R
@@ -0,0 +1,161 @@
+## LCT hybrid panel - data manipulation and hybrid analysis using species-specific allele counting
+#Create 2/29/2024 by MEFL, modified 7/23/2024 by MEFL
+
+#### How to use this script ####
+
+# Set input and output file names as variables in the next code block, then
+#  run the entire script to calculate admixture proportions based on counting
+#  species-specific alleles at loci for LCT, RBT, and YCT
+
+
+
+#### SET 2 VARIABLES ####
+
+## 1. Input file name
+#  - Export called genotypes from software in format "Detailed table results" and list name of csv here
+#  - List file name here with FULL PATH to its location on your computer (pull from Box or from local computer)
+#example: file_in <- "/Users/melanielacava/Library/CloudStorage/Box-Box/GVL/LCT/X9/LCT_HybPanel_Plate1_LaCava20240226/HybPlate1_20240226_DetailedTableResults.csv"
+#example: file_in <- "/Users/melanielacava/Desktop/HybPlate1_20240226_DetailedTableResults.csv" #from local computer
+file_in <- "/Users/melanielacava/Library/CloudStorage/Box-Box/GVL/LCT/X9/X9 Fluidigm Data/LCT Hybrid Panel 202404_ML/LCT_HybridPanel_Plate6_LaCava20240318/LCT_HybridPanel_Plate6_LaCava20240319_DetailedTableResults_NoBadLoci.csv"
+
+
+## 2. Output file name
+#  - List desired output file name with FULL PATH to GVL/LCT/Results folder on Box (or save to your computer, then load to Box)
+#example: file_out <- "/Users/melanielacava/Library/CloudStorage/Box-Box/GVL/LCT/Results/HumboldtBasin_2024/AlleleCountingResults_ByPlate/LCTHybridPanel_Plate1_20240226.csv"
+#example: file_out <- "/Users/melanielacava/Desktop/LCTHybridPanel_Plate1_AlleleCountResults_20240226.csv" #then save to Box
+file_out <- "/Users/melanielacava/Library/CloudStorage/Box-Box/GVL/LCT/X9/X9 Fluidigm Data/LCT Hybrid Panel 202404_ML/LCT_HybridPanel_Plate6_LaCava20240318/NewResults_NoBadLoci.csv"
+
+
+
+#### Import & re-format data ####
+
+##### Genotype data ####
+
+## Import detailed table results csv from genotyping software
+input <- read.csv(file_in, #import csv file designated in code block above
+                  skip=15) #import starting from row 16 where actual data is
+
+## Remove NTC rows and blank assays
+input <- input[!grepl("NTC",input$Type,fixed=T),]
+input <- input[!grepl("Blank",input$Assay,fixed=T),]
+
+## Convert missing data to N:N
+input$Converted[input$Converted=="No Call"] <- "N:N"
+input$Converted[input$Converted=="Invalid"] <- "N:N"
+
+## Split genotype into alleles
+input$allele1 <- substr(input$Converted,1,1)
+input$allele2 <- substr(input$Converted,3,3)
+
+
+
+##### Locus data ####
+
+## Subset input data to desired loci
+#  - the 95 loci in the LCT hybrid panel have been reduced to a set of 58 loci (14 LCT, 32 RBT, 12 YCT)
+#    that passed the validation test - only these loci should be analyzed
+#  - manual list of loci below for easy use of this R script, but alternatively can import text file list of loci
+#loci <- read.table("LCT_Final58loci_2024jul23.txt")[,1] #import text file list of loci
+loci <- c("LCT_06", "LCT_11", "LCT_14", "LCT_25", "LCT_29", "LCT_35", "LCT_46", "LCT_60", "LCT_61", "LCT_70", "LCT_75", "LCT_84",
+          "LCT_87", "LCT_92", "RBT_01", "RBT_07", "RBT_09", "RBT_17", "RBT_21", "RBT_22", "RBT_27", "RBT_28", "RBT_30", "RBT_31",
+          "RBT_32", "RBT_33", "RBT_40", "RBT_41", "RBT_43", "RBT_44", "RBT_45", "RBT_54", "RBT_56", "RBT_62", "RBT_65", "RBT_71",
+          "RBT_74", "RBT_76", "RBT_80", "RBT_81", "RBT_82", "RBT_88", "RBT_89", "RBT_93", "RBT_94", "RBT_95", "YCT_02", "YCT_10",
+          "YCT_12", "YCT_42", "YCT_47", "YCT_49", "YCT_52", "YCT_55", "YCT_66", "YCT_77", "YCT_78","YCT_79")
+input <- input[input$Assay %in% loci,]
+
+
+## List of species-specific alleles at each locus
+#  - manual allele info below for easy use of this R script, but alternatively can import csv allele info
+#  - note that manual list only has alleles for 58 loci, whereas csv has alleles for all 95 loci
+#allele <- read.csv("SpeciesAlleleList.csv") #import species-specific allele info
+allele <- data.frame(Assay=loci,
+                    LCT_allele=c("G", "A", "A", "T", "A", "T", "T", "T", "A", "T", "G", "T", "G", "T", "T", 
+                                 "T", "A", "G", "C", "T", "A", "G", "C", "A", "C", "C", "A", "T", "C", "T", 
+                                "G", "G", "A", "T", "G", "G", "C", "C", "G", "C", "A", "A", "T", "C", "G", 
+                                "G", "G", "G", "C", "G", "C", "C", "T", "G", "T", "G", "C","A"),
+                    RBT_allele=c("A", "G", "G", "C", "G", "G", "C", "C", "G", "C", "T", "C", "A", "G", "A", 
+                                "G", "G", "A", "G", "G", "G", "A", "G", "G", "T", "T", "G", "G", "T", "G", 
+                                "A", "A", "C", "G", "A", "A", "T", "T", "A", "T", "T", "G", "G", "T", "C", 
+                                "T", "G", "G", "C", "G", "C", "C", "T", "G", "T", "G", "C","A"),
+                    YCT_allele=c("A", "G", "G", "C", "G", "N", "C", "C", "G", "C", "T", "C", "A", "G", "T", 
+                                "T", "A", "G", "C", "T", "A", "G", "C", "A", "C", "C", "A", "T", "C", "T", 
+                                "G", "G", "A", "T", "G", "G", "C", "C", "G", "C", "A", "A", "T", "C", "G", 
+                                "G", "A", "T", "T", "T", "T", "G", "A", "T", "C", "A", "A","G"))
+
+
+
+
+
+#### Count species-specific alleles ####
+
+## Add columns to input for each species
+input$LCTcount <- NA
+input$RBTcount <- NA
+input$YCTcount <- NA
+
+## Loop through input to count species-specific alleles
+# 2 = both alleles match the species-specific allele
+# 1 = one allele matches
+# 0 = neither matches 
+# NA = missing data
+
+for (i in 1:nrow(input)){
+  if (input$allele1[i]=="N"){} #skip rows with missing data
+  else{
+    if (grepl("LCT",input$Assay[i])){ #LCT loci
+      input$LCTcount[i] <- (input$allele1[i] == allele$LCT_allele[allele$Assay==input$Assay[i]]) +
+        (input$allele2[i] == allele$LCT_allele[allele$Assay==input$Assay[i]])
+    }
+    if (grepl("RBT",input$Assay[i])){ #RBT loci
+      input$RBTcount[i] <- (input$allele1[i] == allele$RBT_allele[allele$Assay==input$Assay[i]]) +
+        (input$allele2[i] == allele$RBT_allele[allele$Assay==input$Assay[i]])
+    }
+    if (grepl("YCT",input$Assay[i])){ #YCT loci
+      input$YCTcount[i] <- (input$allele1[i] == allele$YCT_allele[allele$Assay==input$Assay[i]]) +
+        (input$allele2[i] == allele$YCT_allele[allele$Assay==input$Assay[i]])
+    }
+  }
+}
+
+
+## Create table of samples to sum species-specific allele counts
+results <- data.frame(sample=sort(unique(input$Name)),
+                   LCTcount=NA,RBTcount=NA,YCTcount=NA, #sum number of species-specific alleles
+                   LCTnonmiss=NA,RBTnonmiss=NA,YCTnonmiss=NA, #sum number of non-missing alleles genotyped
+                   PropMissing=NA, #calc % missing data
+                   LCTprop_AlleleCount=NA,RBTprop_AlleleCount=NA,YCTprop_AlleleCount=NA) #calc admixture prop for each species
+
+for (i in 1:nrow(results)){
+  #sum number of species-specific alleles
+  results$LCTcount[i] <- sum(input$LCTcount[input$Name==results$sample[i]],na.rm=T)
+  results$RBTcount[i] <- sum(input$RBTcount[input$Name==results$sample[i]],na.rm=T)
+  results$YCTcount[i] <- sum(input$YCTcount[input$Name==results$sample[i]],na.rm=T)
+  
+  #sum number of non-missing alleles genotyped
+  results$LCTnonmiss[i] <- sum(!is.na(input$LCTcount[input$Name==results$sample[i]])) * 2
+  results$RBTnonmiss[i] <- sum(!is.na(input$RBTcount[input$Name==results$sample[i]])) * 2
+  results$YCTnonmiss[i] <- sum(!is.na(input$YCTcount[input$Name==results$sample[i]])) * 2
+}
+
+for (i in 1:nrow(results)){
+  #calc missing data %
+  results$PropMissing[i] <- 1 - (rowSums(results[i,c("LCTnonmiss","RBTnonmiss","YCTnonmiss")]) / max(rowSums(results[,c("LCTnonmiss","RBTnonmiss","YCTnonmiss")])))
+  
+  #calc admixture prop for each species (# species-specific alleles / # non-missing alleles genotyped)
+  # EXCLUDING samples with >50% missing data
+  if (rowSums(results[i,c("LCTnonmiss","RBTnonmiss","YCTnonmiss")]) > max(rowSums(results[,c("LCTnonmiss","RBTnonmiss","YCTnonmiss")]))/2) {
+    results$LCTprop_AlleleCount[i] <- results$LCTcount[i]/results$LCTnonmiss[i]
+    results$RBTprop_AlleleCount[i] <- results$RBTcount[i]/results$RBTnonmiss[i]
+    results$YCTprop_AlleleCount[i] <- results$YCTcount[i]/results$YCTnonmiss[i]
+  }
+}
+#View(results)
+
+
+
+#### Export results ####
+write.csv(results[,c("sample","PropMissing","LCTprop_AlleleCount","RBTprop_AlleleCount","YCTprop_AlleleCount")],
+          file_out, #name output based on variable name set at beginning of this script
+          row.names=F,quote=F)
+#SAVE copy of this results csv to Box folder: GVL/LCT/Results/AlleleCountingResults_ByPlate/
+#COPY results into GVL/LCT/Results/Results_CompiledAcrossRuns.xlsx, excluding positive control samples
---
+++ LCT_HybridPanel_CreateStructureFile.R
@@ -0,0 +1,99 @@
+#Convert software output to structure format
+#Created 2/29/2024 by MEFL, updated 7/23/2024 by MEFL
+
+#### How to use this script ####
+
+# Set input and output file names as variables in the next code block, then
+#  run the entire script to create a STRUCTURE-formatted file
+
+# After creating you structure file, open file in a text editor and delete all rows
+#  except for the samples you want to run structure on (keep your positive controls
+#  and any samples that did not come out as 100% one species in your allele counting
+#  analysis)
+
+
+
+#### SET 2 VARIABLES ####
+
+## 1. Input file name
+#  - Export called genotypes from software in format "Detailed table results" and list name of csv here
+#  - List file name here with FULL PATH to its location on your computer (pull from Box or from local computer)
+#example: file_in <- "/Users/melanielacava/Library/CloudStorage/Box-Box/GVL/LCT/X9/LCT_HybPanel_Plate1_LaCava20240226/HybPlate1_20240226_DetailedTableResults.csv"
+#example: file_in <- "/Users/melanielacava/Desktop/HybPlate1_20240226_DetailedTableResults.csv" #from local computer
+file_in <- ""
+
+
+## 2. Desired STRUCTURE file name
+#  - List desired output STRUCTURE file name ending with .stru with FULL PATH
+#example: file_out <- "/Users/melanielacava/Desktop/HybridPlate1_20240226.stru"
+file_out <- ""
+
+
+
+#### Import data ####
+
+## Import detailed table results csv from genotyping software
+input <- read.csv(file_in, #import csv file designated in code block above
+                  skip=15) #import starting from row 16 actual data
+
+## Remove NTC rows and blank assays
+input <- input[!grepl("NTC",input$Type,fixed=T),]
+input <- input[!grepl("Blank",input$Assay,fixed=T),]
+
+## Convert missing data to N:N
+input$Converted[input$Converted=="No Call"] <- "N:N"
+input$Converted[input$Converted=="Invalid"] <- "N:N"
+
+## Split genotype into alleles
+input$allele1 <- substr(input$Converted,1,1)
+input$allele2 <- substr(input$Converted,3,3)
+
+
+##### Locus data ####
+
+## Subset input data to desired loci
+#  - the 95 loci in the LCT hybrid panel have been reduced to a set of 58 loci (14 LCT, 32 RBT, 12 YCT)
+#    that passed the validation test - only these loci should be analyzed
+#  - manual list of loci below for easy use of this R script, but alternatively can import text file list of loci
+#loci <- read.table("LCT_Final58loci_2024jul23.txt")[,1] #import text file list of loci
+loci <- c("LCT_06", "LCT_11", "LCT_14", "LCT_25", "LCT_29", "LCT_35", "LCT_46", "LCT_60", "LCT_61", "LCT_70", "LCT_75", "LCT_84",
+          "LCT_87", "LCT_92", "RBT_01", "RBT_07", "RBT_09", "RBT_17", "RBT_21", "RBT_22", "RBT_27", "RBT_28", "RBT_30", "RBT_31",
+          "RBT_32", "RBT_33", "RBT_40", "RBT_41", "RBT_43", "RBT_44", "RBT_45", "RBT_54", "RBT_56", "RBT_62", "RBT_65", "RBT_71",
+          "RBT_74", "RBT_76", "RBT_80", "RBT_81", "RBT_82", "RBT_88", "RBT_89", "RBT_93", "RBT_94", "RBT_95", "YCT_02", "YCT_10",
+          "YCT_12", "YCT_42", "YCT_47", "YCT_49", "YCT_52", "YCT_55", "YCT_66", "YCT_77", "YCT_78","YCT_79")
+input <- input[input$Assay %in% loci,]
+
+
+
+#### Create structure file ####
+
+## Convert long table to wide genotype matrix
+wide_data <- reshape(input[,c("Assay","Name","allele1","allele2")],idvar="Name",timevar="Assay",direction="wide")
+
+
+## Define a function to convert nucleotide calls to numerical codes
+nucleotide_to_code <- function(genotype) {
+  ifelse(genotype == "A", 1,
+         ifelse(genotype == "T", 2,
+                ifelse(genotype == "C", 3,
+                       ifelse(genotype == "G", 4,
+                              ifelse(genotype == "N", -9, NA)))))  # Encode missing data
+}
+
+## Apply the conversion function to each element of the genotype data
+wide_data_numeric <- wide_data
+wide_data_numeric[,-1] <- apply(wide_data[,-1], MARGIN = 2, FUN = function(col) nucleotide_to_code(col))
+#View(wide_data_numeric)
+
+## Add location data (required column to run structure, but it won't use the info so just = 1 for all)
+wide_data_numeric <- cbind("Name" = wide_data_numeric[, 1], "loc" = 1, wide_data_numeric[, -1])
+
+## Sort rows alphabetically, just to stay organized
+wide_data_numeric <- wide_data_numeric[order(wide_data_numeric$Name),]
+
+## Export structure-formatted file (excluding header)
+write.table(wide_data_numeric,file_out,row.names=F,quote=F,col.names=F)
+
+## Check number of loci and number of samples to input as parameters in structure run
+#nrow(wide_data_numeric) #number of samples
+#ncol(wide_data_numeric)/2 - 1 #number of loci
